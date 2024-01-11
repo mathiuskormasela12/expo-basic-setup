02174c15b949ee05a99ccdd7d24451cbe4bedc0a
Mathius Kormasela <mathiuskormasela12@gmail.com>
2024-01-12 00:23:19 +0700

    update: setup jail-monkey

--- App.tsx
+++ App.tsx
@@ -1,14 +1,35 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import JailMonkey from "jail-monkey";
+import React, { useEffect } from "react";
+import { Alert, BackHandler } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 import Router from "./src/Router";
 import { ReduxWrapper } from "./src/components";
 
 export default function App() {
+  useEffect(() => {
+    if (JailMonkey.isJailBroken()) {
+      Alert.alert(
+        "Warning",
+        "Careful, we've detected that your device has been jailbroken/rooted.",
+        [
+          {
+            text: "Ok",
+            style: "destructive",
+            onPress: () => BackHandler.exitApp(),
+          },
+        ],
+      );
+    }
+  }, []);
+
   return (
-    <ReduxWrapper>
-      <StatusBar style="dark" />
-      <Router />
-    </ReduxWrapper>
+    <SafeAreaProvider>
+      <ReduxWrapper>
+        <StatusBar style="dark" />
+        <Router />
+      </ReduxWrapper>
+    </SafeAreaProvider>
   );
 }
